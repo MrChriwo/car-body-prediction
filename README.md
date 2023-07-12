ca91d6b662abb563a8e4d0c2dab35f65d2a213dc
Christopher Wolf <christopher-wolf@outlook.de>
2023-07-12 17:32:34 +0200

    clean up final adjustments

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 ## Car-Body-Prediction
 
 This project is regarding the standford-cars-dataset which is available on kaggle.
-Scope of this project is to predict the car body of a given image
+Scope of this project is to predict the car size of a given image. 
 
-### Usage to test the model 
+### Team Members 
+      - Dean Tomanelli 
+      - Christopher Wolf 
 
-opeen a terminal inside the project folder. While running docker desktop in background enter the following cmd: 
+### How to test the model 
+
+open a terminal inside the project folder. While running docker desktop in background enter the following cmd: 
 
 ```docker build -t tf_env .```
 
-after the image was successfully build, you can run the container with 
+after the image was successfully built, you can run the container with 
 
 ```docker run -p 8888:8888 --name car_body_prediction tf_env```
 
+To test the Model, you have to run the imports cell. For loading the model, scroll down to the "Load Model" cell.
+
 NOTE: the container provides all dependencies to load the final model (you can ignore the warnings and erros). 
-      because there are some dependencies which requires an nvidia GPU, you will not be able to run the entire model notebook inside the container (i.e. train the model). 
+      because there are some dependencies which require an nvidia GPU, you will not be able to run the entire model notebook inside the container (i.e. train the model). 
+
 
 ### Run the project without container: 
+
 running the project outsite the provided container requires an python environment with all dependencies mentioned in **requirements.txt**
 again, for running the model.ipynb notebook completly you will need to have an nvidia GPU or adjust the code for CPU usage. 
 Because computational benefit, we've used the GPU for the Notebook. 
--- services/Google-Image-Scraper/img_checking.py
+++ services/Google-Image-Scraper/img_checking.py
@@ -1,31 +1,38 @@
-from os import listdir, path, remove, rename 
+from os import listdir, path, remove, rename
 import pickle
 
-def check_images(formats: list) -> dict: 
+def check_images(formats: list) -> dict:
     c = 0
-    dictonary = {}
-    for dir in listdir("./photos"):
-        directory = path.join("./photos", dir)
-        files = listdir(directory)
-        if len(files) > 1: 
-            usefull_files = {dir: []}
-            for fname in files: 
+    # Create an empty dictionary to store useful files
+    dictionary = {} 
+    # Iterate through directories in the "./photos" directory
+    for dir in listdir("./photos"):  
+        directory = path.join("./photos", dir) 
+        files = listdir(directory)  
+        # If there are more than one file in the directory
+        if len(files) > 1:
+            useful_files = {dir: []} 
+            # Iterate through files in the current directory
+            for fname in files:
+                # Check if the file has an extension
                 if "." in fname: 
-                    split = fname.split(".")
-                    if split[-1] in formats: 
-                        usefull_files[dir].append(fname)
-                        c+= 1 
-                        print(f"{c} of {len(files)} successfully checked", end="\r")
-                else:
-                    pth = path.join(directory, fname)
-                    remove(pth)
-            dictonary.update(usefull_files)
-        else: 
-            print(f"{directory} has no files")
+                    split = fname.split(".") 
+                    # Check If the file extension is in the provided formats list
+                    if split[-1] in formats:  
+                        useful_files[dir].append(fname) 
+                        c += 1 
+                        print(f"{c} of {len(files)} successfully checked", end="\r") 
+                else: 
+                    pth = path.join(directory, fname) 
+                    remove(pth)  #
+            dictionary.update(useful_files) 
+        else:
+            print(f"{directory} has no files")  
 
-    with open("picture_struct.pickle", "wb") as file: 
-        pickle.dump(dictonary, file)
-        print("file was created successfully")
+    # safe all to pickle file
+    with open("picture_struct.pickle", "wb") as file:  
+        pickle.dump(dictionary, file) 
+        print("file was created successfully") 
 
 check_images(["jpeg", "png"])
 
